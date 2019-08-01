de7f84008ac8359a0d0d8f0bcffb1d2a6bd18420
Kevon Butler <kevbutler310@gmail.com>
2019-08-01 12:13:20 -0400

    robot

---
+++ src/co/grandcircus/robot.java
@@ -0,0 +1,24 @@
+package co.grandcircus;
+
+public class robot {
+
+	
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getYousuck() {
+			return yousuck;
+		}
+		public void setYousuck(String yousuck) {
+			this.yousuck = yousuck;
+		}
+		private String yousuck;
+		
+		
+	
+
+}
